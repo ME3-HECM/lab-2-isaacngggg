d59988e1826e546176bfca3f93f19c32a23e7588
isaacngggg <108262412+isaacngggg@users.noreply.github.com>
2023-01-21 15:25:04 +0000

    Bitshift version of KITT's grill
    Got it properly done this time, no math library

--- main.c
+++ main.c
@@ -4,39 +4,28 @@
 
 #include <xc.h>
 #include "LEDarray.h"
-#include <math.h>
 #define _XTAL_FREQ 64000000 //note intrinsic _delay function is 62.5ns at 64,000,000Hz  
 
 
 
 void main(void) 
 {
-	unsigned int count=0;
+	unsigned int count= 1;
     LEDarray_init();
     //LDRbutton_init();
     RF2button_init();
     
-    
-    while(count < 511){
-        //while (PORTAbits.RA3 == 0){
-        while (PORTFbits.RF2);                 // whilst no button is pressed keep waiting
-        if(!PORTFbits.RF2) {                     // if button is pressed 
-            count ++;                           // count up
-            LEDarray_disp_bin(count);           // light up the amount counted
-            __delay_ms(100); 
-            while (!PORTFbits.RF2){
-                __delay_ms(300); 
-                if(!PORTFbits.RF2) {
-                    count ++;
-                    LEDarray_disp_bin(count);
-                }
-                else{break;}
-            }
+   
+    while(1){
+        while (count != 256){
+            count = count << 1; // bitshift 1
+            LEDarray_disp_bin(count); //output a on the LED array in binary
+            __delay_ms(100); // Delay so human eye can see change
         }
-        
-        //}
-        //if(count > 127) {count=0;}
-        //LEDarray_disp_bin(count); //output a on the LED array in binary
+        while (count != 1) {
+            count = count >> 1; // right shift
+            LEDarray_disp_bin(count); //output a on the LED array in binary
+            __delay_ms(100); // Delay so human eye can see change
+        }
     }
-}
-
+}   
